ad712e78e986359febc8e733df5e3ee08b3ebcb1
richardvogg <richard.vogg@web.de>
2021-11-01 16:50:54 +0100

    Finished day 1

--- day01_points/day01_points.R
+++ day01_points/day01_points.R
@@ -24,8 +24,6 @@ df <- read.csv("../datasets/Earthquakes Chile Nov 2021.csv") %>%
   )
 
 
-#88CCEE,#CC6677,#DDCC77,#117733,#332288,#AA4499,#44AA99,#999933,#882255,#661100,#6699CC,#888888
-
 seacolor <- "#8ecae6"
 textcol <- "#023047"
 small <- "#ffb703"
@@ -36,29 +34,68 @@ ggplot() +
           fill="grey95", col = "grey80")+
   geom_point(data=subset(df, mag <= 7.5),
              aes(x = longitude, y = latitude), size = 2, col=small, alpha=0.1)+
-  geom_point(data=subset(df, mag > 7.5),
-             aes(x = longitude, y = latitude), col = big, size = 3)+
   geom_text_repel(data=subset(df, mag>7.5 & longitude<(-70) & latitude>=(-40)),
-                  aes(x = longitude,y=latitude,label=paste(year,"|",mag,"\n",region)),
-                  nudge_x = -7 , col=textcol , family="Piazzolla", segment.colour = textcol)+
+                  aes(x = longitude,y=latitude,label=paste(year,"\n",toupper(region))),
+                  nudge_x = -9 , col=textcol , family="Piazzolla", size = 3.5)+
   geom_text_repel(data=subset(df, mag>7.5 & (longitude > (-70) | latitude < (-40))),
-                  aes(x = longitude, y = latitude, label = paste(year,"|",mag,"\n",region)),
-                  nudge_x = 6, col=textcol, family="Piazzolla", segment.colour = textcol)+
-  geom_richtext(data = NULL, aes(x = -69, y = -32, 
-                                 label = glue::glue("**Earthquakes <br> in Chile <br>
-                                                    in the 21st <br> century**")), 
-                hjust = 0, fill = NA, label.color = NA, size = 6, family = "Piazzolla", color = textcol) +
-  scale_size_continuous(breaks = c(5,6,7,8.8),range = c(0.5,3))+
+                  aes(x = longitude, y = latitude, label = paste0(year,"\n",toupper(region))),
+                  nudge_x = 8, col=textcol, family="Piazzolla", size = 3.5)+
+  geom_point(data=subset(df, mag > 7.5),
+             aes(x = longitude, y = latitude, size = mag), col = big)+
+  geom_text(data=subset(df, mag > 7.5),
+             aes(x = longitude, y = latitude, label = mag), size = 3, col = "#b35f00")+
+  
+  scale_size_continuous(breaks = c(7.5, 8.8), range = c(8, 15))+
   labs(x="",y="",
+       title = "Earthquakes in Chile in the 21st century",
+       subtitle="Events over 7.5 magnitude highlighted",
        caption="Data from: https://earthquake.usgs.gov/earthquakes/search/")+
   theme_light()+
   theme(legend.position="none",
         text = element_text(family = "Piazzolla", colour = textcol),
         plot.background = element_rect(fill = "grey90"),
+        panel.grid = element_blank(),
         panel.background = element_rect(fill=seacolor),
         plot.title = element_text(face = "bold", size = 16),
         plot.subtitle = element_text(size=14),
         plot.caption = element_text(size=10),
         axis.text=element_blank(),
         axis.ticks=element_blank())+
-  coord_quickmap(xlim = c(-85,-60), ylim = c(-56,-17))
+  coord_quickmap(xlim = c(-90,-55), ylim = c(-56,-17))
+
+
+
+
+
+### South America
+
+df <- read.csv("../datasets/earthquakes_nov_2021.csv", skip = 1, sep = ";") %>% 
+  mutate(year=substr(time,1,4)) %>%
+  select(-X)
+
+ggplot() + 
+  borders(regions=c("Chile","Argentina","Peru", "Bolivia", "Paraguay", "Brazil",
+                    "Venezuela", "Colombia", "Uruguay", "Ecuador", "Guyana",
+                    "Suriname", "French Guiana", "Panama", "Costa Rica", "Nicaragua"), 
+          fill="grey95", col = "grey80")+
+  geom_point(data=subset(df, mag <= 7.5),
+             aes(x = longitude, y = latitude), size = 1, col=small, alpha=0.1)+
+  geom_point(data=subset(df, mag > 7.5),
+             aes(x = longitude, y = latitude), col = big, size = 3) +
+  geom_richtext(data = NULL, aes(x = -60, y = -10, 
+                                 label = glue::glue("**Earthquakes <br> in South America <br>
+                                                    in the 21st <br> century**")), 
+                hjust = 0, fill = NA, label.color = NA, size = 6, family = "Piazzolla", color = textcol) +
+  theme_light()+
+  theme(legend.position="none",
+        text = element_text(family = "Piazzolla", colour = textcol),
+        plot.background = element_blank(),
+        panel.grid = element_blank(),
+        panel.background = element_rect(fill=seacolor),
+        plot.title = element_text(face = "bold", size = 16),
+        plot.subtitle = element_text(size=14),
+        plot.caption = element_text(size=10),
+        axis.text=element_blank(),
+        axis.ticks=element_blank())+
+  coord_quickmap(xlim = c(-85,-30), ylim = c(-56,10))
+  

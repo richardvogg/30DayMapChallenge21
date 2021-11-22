50c48495150f4228d8246a0c270c0bf5fbd947d2
richardvogg <richard.vogg@web.de>
2021-11-22 19:15:27 +0100

    save image

--- day21_elevation/day21_elevation.R
+++ day21_elevation/day21_elevation.R
@@ -21,7 +21,7 @@ canary <- st_bbox(c(xmin = -18.35, ymin = 27.39, xmax = -13.32, ymax = 29.52), c
 canary_df <- rasterToPoints(canary) %>% data.frame()
 colnames(canary_df) <- c("lon", "lat", "alt")
 
-canary_df %>%
+map <- canary_df %>%
   mutate(alt = ifelse(alt < 0 , -1000, alt)) %>%
 ggplot(aes(lon, lat)) + 
   geom_contour_filled(aes(z = alt), breaks = seq(-1000, 3700, 100)) +
@@ -29,7 +29,17 @@ ggplot(aes(lon, lat)) +
     annotate("text", -16, 29.2, label = "The Canary Islands", size = 16, 
              fontface = "bold", color = "#FEB87EFF", hjust = 0.5, family = "Pacifico") +
   coord_quickmap() +
-  theme_void() +
+  theme_void()
+
+legend <- data.frame(id = 1:47) %>%
+  ggplot(aes(x = id, y = 1, fill = id)) + 
+  geom_tile(col = "grey") +
+  geom_text(aes(x = 50, y = 1, label = "3715 m"), col = "#FCFDBFFF", hjust = 0) +
+  scale_fill_viridis_c(option = "magma") +
+  xlim(10, 65) + 
+  theme_void()
+
+map + inset_element(legend, 0.75, 0.02, 0.9, 0.05) &
   theme(legend.position = "none",
         plot.background = element_rect(fill = "black"))
 

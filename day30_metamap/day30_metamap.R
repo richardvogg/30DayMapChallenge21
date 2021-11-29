f3280c91df1af9e38ca7a8b719675b1bb4a93be8
richardvogg <richard.vogg@web.de>
2021-11-29 15:03:52 +0100

    added day 30

--- day30_metamap/day30_metamap.R
+++ day30_metamap/day30_metamap.R
@@ -3,17 +3,17 @@ library(leaflet)
 
 leaflet() %>%
   addProviderTiles(provider = providers$Stamen.TonerBackground) %>%
-  addRectangles(-160,-60,160,80) %>%
+  addRectangles(-160,-60,160,80) %>% # World
   addRectangles(-99.36,19.05,-98.94,19.59, label="Mexico City: 22",
                 labelOptions = labelOptions(noHide = T,direction="left")) %>%
   addRectangles(-82,-58,-30,15, label="South America: 15",
                 labelOptions = labelOptions(noHide = T,direction="right")) %>%
-  addRectangles(-26,35,37.5,70) %>%
+  addRectangles(-26,35,37.5,70) %>% # Europe
   addRectangles(-75.64, -55.61, -66.96, -17.58,label="Chile: 1,12",
                 labelOptions = labelOptions(noHide = T,direction="right")) %>%
   addRectangles(5.99, 47.30, 15.02, 54.98, label="Germany: 2,5,17,18,23",
                 labelOptions = labelOptions(noHide = T,direction="right")) %>%
-  addRectangles(-19,-35,51,38) %>%
+  addRectangles(-19,-35,51,38) %>% # Africa
   addRectangles(-16.9,27.9,-16.1,28.7,label="Canary Islands: 19,20",
                 labelOptions = labelOptions(noHide = T,direction="left")) %>%
   addRectangles(2.22,48.82,2.47,48.90,label="Paris: 9",
